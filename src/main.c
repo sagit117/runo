e0ff4a9ee12e0554354b44467f3711b580913676
axel <sagit117@gmail.com>
2024-11-13 11:52:13 +0300

    man app

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 #include "headers/logger.h"
+#include <string.h>
 
 Logger logger = {.level = ALL};
 
-int main(void) {
-    log_error("test");
-    log_debug("test");
-    log_info("test");
-    log_warning("test");
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        log_error("Укажите параметр конфигурации и путь до файла конфигурации, пример: -c ./cfg");
+        return 0;
+    } 
+
+    if (strcmp("-c", argv[1]) != 0 && strcmp("-h", argv[1]) != 0) {
+        log_error("Не известный параметр");
+        return 0;
+    }
+
+    if (strcmp("-h", argv[1]) == 0) {
+        log_info("\nПараметры приложения: \n   -c: параметр конфигурации и путь до файла конфигурации, пример: -c ./cfg");
+    }
+
     return 0;
 }
