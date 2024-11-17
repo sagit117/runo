6e6b32ca3b0246cb90af3cf44e1047a00f0c7bc9
axel <sagit117@gmail.com>
2024-11-17 16:09:37 +0300

    server send

--- src/server/http/runo_http_server.c
+++ src/server/http/runo_http_server.c
@@ -2,27 +2,12 @@
 #include "../../headers/logger.h"
 #include "../../headers/common.h"
 
-#include <sys/socket.h> // socket
-// #include <sys/types.h>
+#include <sys/socket.h> // socket accept
 #include <netinet/in.h> // sockaddr_in
 #include <unistd.h> // close
-#include <string.h>
-#include <ctype.h>
-
-void processing_message(unsigned char *s, int n) {
-    log_debug("processing message...\n");
-    unsigned char *p;
-    for (p=s; p < s + n; p++)
-    {
-        if (islower(*p))
-        {
-            *p += 13;
-            if(*p > 'z') *p -= 26;
-        }
-    }
-}
 
 BOOL runo_http_server(Server *config_server) {
+    // Создание сокета
     int server_socket = socket(AF_INET, SOCK_STREAM, 0); // AF_INET - ip4
 
     if (server_socket == -1) {
@@ -30,46 +15,51 @@ BOOL runo_http_server(Server *config_server) {
         return FALSE;
     }
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(config_server->port);
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Опичание структуры адреса сокета
+    struct sockaddr_in server_address = {
+        .sin_family = AF_INET, // AF_INET - ip4
+        .sin_port = htons(config_server->port),
+        .sin_addr.s_addr = htonl(INADDR_ANY)
+    };
 
-    struct sockaddr_in client_address;
-    socklen_t client_address_len;
-
+    // Связать сокет с адресом в выбранном домене
     int rc = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
     if (rc < 0) {
-        log_error("Ошибка открытия вызова команды bind server_address");
+        log_error("Ошибка связки сокета с адресом");
         return FALSE;
     }
 
-    listen(server_socket, config_server->max_connections);
-
-    log_info("Сервер ожидает подключения...");
+    listen(server_socket, config_server->max_connections); // max_connections - количетсво в очереде ожидающих запросов
+    log_info("Сервер ожидает подключения, порт: %d", config_server->port);
     
-    // ACCEPT connections
+    // Прием соединений
+    struct sockaddr_in client_address;
+    socklen_t client_address_len;
+
     int keep_socket = 1;
+
     while (keep_socket) {
         client_address_len = sizeof(client_address);
 
-        int client_socket;
-        client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_address_len);
+        int client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_address_len);
+        if (client_socket < 0) {
+            log_error("Ошибка принятия запроса");
+            perror("accept");
 
-        log_debug("Подключен клиент с адресом %d", client_address.sin_addr.s_addr);
+            continue;
+        } else {
+            log_debug("Подключен клиент с адресом: %d, порт: %d", client_address.sin_addr.s_addr, client_address.sin_port);
 
-        // send the message
-        log_debug("processing message service...");
-        log_debug("fd is: %f", client_socket);
-        unsigned char buf[1024]; // Pre-allocated buffer
-        int count;
+            char buf[1024];
+            int recieve_bytes = recv(client_socket, buf, sizeof(&buf), 0);
 
-        while ((count = read(client_socket, buf, 1024)) > 0) {
-            log_debug("%p\n",count);
-            log_debug("reading...");
-            processing_message(buf, count);
-            //send(out, buf, count, 0);
-            write(client_socket, buf, count);
+            log_debug("Прочитано %d байт", recieve_bytes);
+            log_debug("Прочитано %s", buf);
+
+            // send the message
+            int send_bytes = send(client_socket, "HTTP/1.1 200 OK\nContent-Type: text/html", sizeof("HTTP/1.1 200 OK\nContent-Type: text/html"), 0);
+
+            log_debug("Отправлено %d байт", send_bytes);
         }
 
         // close the socket
